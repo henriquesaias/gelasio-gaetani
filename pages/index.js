511ebf80abf2aa69b8e6cd4420c5f90c471c7567
Henrique Saias <henriquesaias@hsaias.com>
2023-12-04 19:10:13 +0000

    carlito no link + D' with no space

--- pages/index.js
+++ pages/index.js
@@ -45,46 +45,45 @@ export default function Landing() {
 
   return (
     <>
-      <main style={
-        {
+      <main
+        style={{
           fontFamily: '"eb garamond", serif',
           lineHeight: 1.4
-        }
-      }
+        }}
       >
-
         <h1 className="text-3xl"
-          style={
-            {
-              letterSpacing: 3,
-              marginTop: '1em',
-              textAlign: 'center'
-            }
-          }
-        >Gelasio Gaetani D 'Aragona</h1>
+          style={{
+            letterSpacing: 3,
+            marginTop: '1em',
+            textAlign: 'center'
+          }}
+        >
+          Gelasio Gaetani D'Aragona
+        </h1>
 
         <div className="flex flex-wrap justify-center">
-          <img src='main.webp'
+          <img
+            src='main.webp'
             width={265}
             style={{ margin: '2em 0' }}
           />
         </div>
 
-        <div style={
-          {
+        <div
+          style={{
             display: 'flex',
             justifyContent: 'center'
-          }
-        }>
-          <div style={
-            {
+          }}
+        >
+          <div
+            style={{
               textAlign: 'center',
               marginBottom: '2em',
               fontSize: 19,
               width: 600,
               maxWidth: '96vw'
-            }
-          }>
+            }}
+          >
             <p>Specialists in the world of Italian wines.</p>
             <p>From property acquisition or sale to wine making and promoting.< br />
               Marketing for USA, Northern Europe, UK, China, and other growing markets.</p>
@@ -92,14 +91,17 @@ export default function Landing() {
             <br />
             <p>Our Philosophy is to disclose the story behind great wines.<br />
               From biological and cultural lineage to modern day company ethos and methods.</p>
-            <p>Please <a href='#contact' style={{textDecoration: 'underline'}}>contact us</a> for further information.</p>
-            <p>artwork by <a href="https://www.carlitodalceggio.com"
-              target="_blank"
-              style={
-                {
-                  textDecoration: 'underline'
-                }
-              }> Carlito Dalceggio </a></p>
+            <p>Please <a href='#contact' style={{ textDecoration: 'underline' }}>contact us</a> for further information.</p>
+            <p style={
+              {
+                opacity: .7,
+                marginTop: '2em',
+                float: 'right',
+                fontSize: 'small'
+              }
+            }>
+              artwork by Carlito Dalceggio
+            </p>
           </div>
         </div>
       </main>
@@ -115,76 +117,95 @@ export default function Landing() {
                 }
               } >
                 <div className="flex-auto p-5 lg:p-10" >
-                  <h4 id='contact'
-                    className="text-2xl font-semibold text-center" >
-                    Contact </h4> <p className="leading-relaxed mt-1 mb-4 text-blueGray-500 text-center">
-                    Email: <a href="mailto:g.gelasio@gmail.com"
-                      style={
-                        {
-                          textDecoration: 'underline'
-                        }
-                      } > g.gelasio @gmail.com </a> </p> <div className="relative w-full mb-3 mt-8" >
-                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="full-name" >
-                      Name </label> <input type="text"
-                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full"
-                        placeholder="Name"
-                        value={
-                          name
-                        }
-                        onChange={
-                          (event) => handleChangeName(event)
-                        }
-                        disabled={
-                          submitted
-                        }
-                    /> </div>
+                  <h4
+                    id='contact'
+                    className="text-2xl font-semibold text-center"
+                  >
+                    Contact
+                  </h4>
+                  <p className="leading-relaxed mt-1 mb-4 text-blueGray-500 text-center">
+                    Samba Gaetani<br />
+                    <a
+                      href="https://wa.me/393355623329"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      +39 335 562 3329
+                    </a><br />
+                    <a
+                      href="mailto:sambagaetani@gmail.com"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                      style={{ textDecoration: 'underline' }}
+                    >
+                      sambagaetani@gmail.com
+                    </a>
+                  </p>
 
-                  <div className="relative w-full mb-3" >
-                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="email" >
-                      Email </label> <input type="email"
-                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full"
-                        placeholder="Email"
-                        value={
-                          email
-                        }
-                        onChange={
-                          (event) => handleChangeEmail(event)
-                        }
-                        disabled={
-                          submitted
-                        }
-                    /> </div>
+                  <div className="relative w-full mb-3 mt-8" >
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="full-name"
+                    >
+                      Name
+                    </label>
+                    <input type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full"
+                      placeholder="Name"
+                      value={name}
+                      onChange={(event) => handleChangeName(event)}
+                      disabled={submitted}
+                    />
+                  </div>
 
                   <div className="relative w-full mb-3">
-                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="message" >
-                      Message </label> <textarea rows="4"
-                        cols="80"
-                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full"
-                        placeholder="Your message..."
-                        value={
-                          message
-                        }
-                        onChange={
-                          (event) => handleChangeMessage(event)
-                        }
-                        disabled={
-                          submitted
-                        }
-                    /> </div> <div className="text-center mt-6">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input type="email"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(event) => handleChangeEmail(event)}
+                      disabled={submitted}
+                    />
+                  </div>
+
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="message"
+                    >
+                      Message
+                    </label>
+                    <textarea rows="4"
+                      cols="80"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow w-full"
+                      placeholder="Your message..."
+                      value={message}
+                      onChange={(event) => handleChangeMessage(event)}
+                      disabled={submitted}
+                    />
+                  </div>
+                  <div className="text-center mt-6">
                     <button className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      style={
-                        {
-                          backgroundColor: userFeedback === 'Email sent' && 'darkgreen'
-                        }
-                      }
+                      style={{
+                        backgroundColor: userFeedback === 'Email sent' && 'darkgreen'
+                      }}
                       type="button"
-                      onClick={
-                        () => sendMail()
-                      } > {
-                        userFeedback
-                      } </button> </div> </div> </form> </div> </div> </div> </div> </>
+                      onClick={() => sendMail()}>
+                      {userFeedback}
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
