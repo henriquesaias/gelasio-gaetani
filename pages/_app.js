c9a432b8c2325aa2a80c77e2c5390fa2c9add2dd
Henrique Saias <henriquesaias@hsaias.com>
2023-12-16 13:25:18 +0000

    remove duplicate meta tags, add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "next/app";
-import Head from "next/head";
-
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "styles/tailwind.css";
 
@@ -13,21 +10,9 @@ export default class MyApp extends App {
     const Layout = Component.layout || (({ children }) => <>{children}</>);
 
     return (
-      <React.Fragment>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>Gelasio Gaetani D'Aragona</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet" />
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </React.Fragment>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     );
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="canonical" href="https://gelasiogaetani.com" />
           <meta name="description" content="Specialists in the world of Italian wines. From biological and cultural lineage to modern day company ethos and methods." />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/main.webp" />
         </Head>
         <body className="text-blueGray-700 antialiased">
           <Main />
